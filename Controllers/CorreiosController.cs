07fb09fd672538e5e72bc95ef0678a6341484c8e
Matheus Silva <job.matheussilva@gmail.com>
2020-10-08 00:33:11 -0300

    Initial commit

---
+++ Controllers/CorreiosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class CorreiosController : ApiController
+    {
+        [Route("api/correios/{cep}")]
+        [HttpGet, HttpPost]
+        public HttpResponseMessage GetCEP(string cep)
+        {
+            var correios = new CorreiosService.AtendeClienteClient();
+            var consulta = correios.consultaCEP(cep);
+            return Request.CreateResponse<Object>(HttpStatusCode.OK, consulta);
+        }
+
+    }
+}
